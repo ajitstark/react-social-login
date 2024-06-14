d7b2a85cd3d5cd580a5e354ed553de82037e5641
Ajit Forger <amazingajit7@gmail.com>
2024-06-14 15:08:25 +0530

    main

--- src/SocialLogin.jsx
+++ src/SocialLogin.jsx
@@ -121,7 +121,7 @@ const REDIRECT_URI = 'https://react-social-login-nu.vercel.app';
           <LoginSocialInstagram
             client_id="1382374749124081"
             client_secret="de219cc3ade4a73ec82dead7fb7f100f"
-            redirect_uri="https://react-social-login-nu.vercel.app"
+            redirect_uri="https://react-social-login-nu.vercel.app/"
             onLoginStart={onLoginInstagramStart}
             onLogoutSuccess={onLogoutInstagramSuccess}
             onResolve={({ provider, data }) => {
